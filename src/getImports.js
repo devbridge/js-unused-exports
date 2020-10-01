bcca927c6312c0fe2543c076380e919430fcb513
Kent Andersen <kent.andersen@bekk.no>
2020-10-01 11:59:11 +0200

    Improved getImportDetails

--- src/getImports.js
+++ src/getImports.js
@@ -3,6 +3,17 @@ import path from 'path';
 import resolve from 'enhanced-resolve';
 import { parse } from '@babel/parser';
 import traverse from '@babel/traverse';
+import {
+  isImportNamespaceSpecifier,
+  isImportDefaultSpecifier,
+  isMemberExpression,
+  isIdentifier,
+  isImportDeclaration,
+  isExportAllDeclaration,
+  isExportNamedDeclaration,
+  isExportSpecifier,
+  isImportSpecifier,
+} from '@babel/types';
 import { getExportedIdentifiers } from './getExports';
 import { toRelativePath } from './utils';
 
@@ -15,19 +26,7 @@ export default function getImports(sourcePaths, ctx) {
       const source = fs.readFileSync(sourcePath, 'utf8');
       const relativePath = toRelative(sourcePath);
       const importData = getImportData(source, sourcePath, ctx);
-
-      const imports = importData.reduce((acc, { from, specifiers }) => {
-        if (!acc[from]) {
-          acc[from] = specifiers;
-        } else {
-          // Duplicate import from the same path
-          acc[from] = acc[from].concat(specifiers);
-        }
-
-        acc[from].sort();
-
-        return acc;
-      }, {});
+      const imports = groupImports(importData);
 
       return {
         sourcePath,
@@ -38,10 +37,25 @@ export default function getImports(sourcePaths, ctx) {
     .filter(({ imports }) => Object.keys(imports).length);
 }
 
+function groupImports(importData) {
+  return importData.reduce((acc, { from, specifiers }) => {
+    if (!acc[from]) {
+      acc[from] = specifiers;
+    } else {
+      // Duplicate import from the same path
+      acc[from] = acc[from].concat(specifiers);
+    }
+
+    acc[from].sort();
+
+    return acc;
+  }, {});
+}
+
 function getImportData(source, srcPath, ctx) {
   const { config } = ctx;
   const ast = parse(source, config.parserOptions);
-  const nodes = ast.program.body.filter(isImportDeclaration);
+  const nodes = ast.program.body.filter(isImport);
 
   return nodes
     .map((node) => getImportDetails(node, srcPath, ast, ctx))
@@ -57,97 +71,115 @@ function isIgnoredImport(importPath, ctx) {
   );
 }
 
-function isImportDeclaration(node) {
+function isImport(node) {
   return (
-    node.type === 'ImportDeclaration' ||
-    // When import is used together with export:
-    // export { default } from './my-file';
-    ((node.type === 'ExportNamedDeclaration' ||
-      node.type === 'ExportAllDeclaration') &&
-      node.source &&
-      node.source.value)
+    (isImportDeclaration(node) ||
+      // When import is used together with export:
+      // export { default } from './my-file';
+      isExportNamedDeclaration(node) ||
+      // export * from './my-file';
+      isExportAllDeclaration(node)) &&
+    node.source &&
+    node.source.value
   );
 }
 
-export function getImportDetails(node, srcPath, ast, ctx) {
-  const createFilter = (type) => (specifier) => specifier.type === type;
-  const isNamespaceSpecifier = createFilter('ImportNamespaceSpecifier');
-  const isDefaultSpecifier = createFilter('ImportDefaultSpecifier');
-  const isImportSpecifier = createFilter('ImportSpecifier');
-  const isExportSpecifier = createFilter('ExportSpecifier');
-
-  const { specifiers = [] } = node;
+function getNamespaceSpecifierNames(node, ast) {
+  if (!node.specifiers) return [];
 
   // Get namespace imports:
   // import * as something from './somewhere'
-  const specifierNodes = specifiers.filter(isNamespaceSpecifier);
-  const nsSpecifiers = [];
+  const specifierNodes = node.specifiers.filter(isImportNamespaceSpecifier);
 
   // Get identifiers that are used from this namespace
   // May not be acurate
-  if (specifierNodes.length) {
-    const nsMap = specifierNodes.reduce(
-      (accumulator, specifierNode) =>
-        accumulator.set(specifierNode.local.name, true),
-      new Map()
-    );
+  if (!specifierNodes.length) return [];
 
-    traverse(ast, {
-      enter(nodePath) {
-        if (
-          nodePath.node.type === 'MemberExpression' &&
-          nodePath.node.object.type === 'Identifier' &&
-          nsMap.has(nodePath.node.object.name)
-        ) {
-          nsSpecifiers.push(nodePath.node.property.name);
-        }
-      },
-    });
+  const namespaceSpecifierNames = [];
+  const localSpecifierNames = specifierNodes.map(
+    (specifierNode) => specifierNode.local.name
+  );
+
+  traverse(ast, {
+    enter({ node: currentNode }) {
+      if (
+        isMemberExpression(currentNode) &&
+        isIdentifier(currentNode.object) &&
+        localSpecifierNames.includes(currentNode.object.name)
+      ) {
+        namespaceSpecifierNames.push(currentNode.property.name);
+      }
+    },
+  });
+
+  return namespaceSpecifierNames;
+}
+
+function getDefaultSpecifierNames(node) {
+  if (!node.specifiers) return [];
+  if (node.specifiers.some(isImportDefaultSpecifier)) {
+    return ['default'];
+  }
+  return [];
+}
+
+export function getImportDetails(node, srcPath, ast, ctx) {
+  if (!node.source || !node.source.value) {
+    return { specifiers: [] };
+  }
+  const { source, specifiers = [] } = node;
+  const from = resolvePath(source.value, srcPath, ctx);
+
+  // Case: import sampleFile, { firstName } from './sample-file';
+  if (isImportDeclaration(node)) {
+    const specifierNames = specifiers
+      .filter(isImportSpecifier)
+      .map((specifier) => specifier.imported && specifier.imported.name)
+      .filter(Boolean);
+
+    return {
+      from,
+      specifiers: [
+        ...getDefaultSpecifierNames(node, ast),
+        ...specifierNames,
+        ...getNamespaceSpecifierNames(node, ast),
+      ],
+    };
   }
 
-  const specifierNames = specifiers
-    .filter(isImportSpecifier)
-    .map((specifier) => specifier.imported && specifier.imported.name)
-    .filter(Boolean);
-
-  if (specifiers.some(isDefaultSpecifier)) {
-    specifierNames.push('default');
-  }
-
-  // Case: export { default } from './sample-file';
-  if (
-    node.type === 'ExportNamedDeclaration' &&
-    node.source &&
-    node.source.value
-  ) {
-    const localNamedExports = node.specifiers
+  // Case: export { default as sampleFile, firstName } from './sample-file';
+  if (isExportNamedDeclaration(node)) {
+    const specifierNames = specifiers
       .filter(isExportSpecifier)
       .map((specifier) => specifier.local.name);
 
-    specifierNames.push(...localNamedExports);
+    return {
+      from,
+      specifiers: specifierNames,
+    };
   }
 
   // Case: export * from './sample-file';
-  if (
-    node.type === 'ExportAllDeclaration' &&
-    node.source &&
-    node.source.value
-  ) {
+  if (isExportAllDeclaration(node)) {
     const { config } = ctx;
 
     const sourcePath = resolvePath(node.source.value, srcPath, ctx);
     const source = fs.readFileSync(sourcePath, 'utf8');
 
-    const allExportNames = getExportedIdentifiers(
+    const specifierNames = getExportedIdentifiers(
       source,
       config.parserOptions
     ).map(({ name }) => name);
-    specifierNames.push(...allExportNames);
+
+    return {
+      from,
+      specifiers: specifierNames,
+    };
   }
 
   return {
-    specifiers: [...specifierNames, ...nsSpecifiers],
-    from: resolvePath(node.source.value, srcPath, ctx),
+    from,
+    specifiers: [],
   };
 }
 
